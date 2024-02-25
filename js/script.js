9794831583548352080fad725fa1eb3349c5b7c1
kinejeune2 <159565893+kinejeune2@users.noreply.github.com>
2024-02-25 17:23:17 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function register() {
+    // Action pour le bouton d'inscription
+    alert("Redirect to registration page");
+}
+
+function login() {
+    // Action pour le bouton de connexion
+    alert("Redirect to login page");
+}
